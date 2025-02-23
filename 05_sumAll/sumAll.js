96910cdc56378bef5a078cc1d87fa7b825ab308f
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-02-23 09:51:07 +0800

    Solve challenges

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let larger = 0;
+  let lesser = 0;
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  if (num1 <= num2) {
+    larger = num2;
+    lesser = num1;
+  } else {
+    larger = num1;
+    lesser = num2;
+  }
+  for (lesser; lesser <= larger; lesser++) {
+    sum += lesser;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (!(year % 100) && !(year % 400)) {
+        return true
+    } else if (!(year % 4) && (year % 100)) {
+        return true
+    }
+    return false;
 };
 
 // Do not edit below this line
