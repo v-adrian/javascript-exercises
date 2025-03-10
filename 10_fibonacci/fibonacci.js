be99a60b6a7659f4d8cc51227c19ca63f0586267
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-03-10 23:48:51 +0800

    Solve challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = (index) => {
+  let num = +index;
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+  let a = 0;
+  let b = 1;
+  let c;
+  for (let i = 2; i <= num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 };
 
 // Do not edit below this line
