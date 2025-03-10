bfbebccfe604ce1f039b99a749fb0db2b2e3b6b3
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-03-11 00:40:55 +0800

    Solve challenges

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+  return people.sort((personA, personB) => {
+    const ageA =
+      (personA.yearOfDeath || new Date().getFullYear()) - personA.yearOfBirth;
+    const ageB =
+      (personB.yearOfDeath || new Date().getFullYear()) - personB.yearOfBirth;
+    return ageB - ageA;
+  })[0];
 };
 
 // Do not edit below this line
