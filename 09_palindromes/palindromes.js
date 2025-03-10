1ef1fa879f42971da9e4a785e32cd6fd2cc3b5f6
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-03-10 23:19:08 +0800

    Solve challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const string = Array.from(str.toLowerCase()).filter((letter) =>
+    letter.match("[a-z0-9]")
+  );
+  const reverseString = string.toReversed();
+  return string.every((value, index) => value === reverseString[index]);
 };
 
 // Do not edit below this line
